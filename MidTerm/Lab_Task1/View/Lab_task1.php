ee747c41de3753f069787047f657833b020179e7
S4KIB <131855103+S4KIB@users.noreply.github.com>
2025-07-23 09:27:22 +0600

    Add files via upload

---
+++ MidTerm/Lab_Task1/View/Lab_task1.php
@@ -0,0 +1,60 @@
+<DOCTYPE html>
+    <html lang = "en">
+        <head>
+            <meta charset = "UTF-8">
+            <meta name = "viewpoint" content = "width = device-width, initial-scale=1.0">
+            <title>This is Registration Form</title>
+        </head>
+
+        <body>
+            <center>
+
+            <h1>AIUB</h1>
+            <h2>Course Registration Form</h2>
+
+            </center>
+
+            <h3 align ="left">Start The Registration & Fill The Form</h3>
+
+
+            <table>
+                <tr>
+                    <td>Email:</td>
+                    <td><input type = "text"></td>
+                </tr>
+
+                <tr>
+                    <td>Password:</td>
+                    <td><input type = "text"></td>
+                </tr>
+
+                <tr>
+                    <td>Select The Gender</td>
+                    <td>
+                        <input type ="radio" name="gender">Male
+                        <input type ="radio" name="gender">Female
+                        <input type ="radio" name="gender">Other
+                    </td>
+                </tr>  
+                
+                <tr>
+                    <td>Upload Your Photo:</td>
+                    <td><input type = "Date"></td>
+                </tr>  
+                
+                <tr> 
+                    <td>Comments:</td>
+                    <td><textarea rows="4" cols="40"></textarea></td>
+                </tr>
+
+                <tr> 
+                    <td><input type="Submit" value="Register"></td>
+                </tr>
+
+            </Table>
+
+        </bady>
+
+    </html>
+
+
