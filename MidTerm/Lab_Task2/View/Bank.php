14d72e1269d7582cc94aa69448a1e4c401dd4fcf
S4KIB <sakib.abir78@gmail.com>
2025-07-26 22:32:58 +0600

    New Files

---
+++ MidTerm/Lab_Task2/View/Bank.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>This is Registration Form</title>
+ 
+<body style="background-color: lightblue;">
+<center>
+<h1 style ="color: blue;">Bank Management System</h1>
+<h2 style ="color: blue;">Your Trusted Financial  Partner</h2>
+</center>
+<h3 align="left"> Customer Registration Form</h3>
+<table style="background-color: white;">
+
+<tr>
+<td>Fullname:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Date of Birth</td>
+<td><input type="date"></td>
+</tr>
+
+<tr>
+<td>Gender:</td>
+<td>
+<input type="radio" name="gender">Male
+<input type="radio" name="gender">Female
+<input type="radio" name="gender">Other
+</td>
+</tr>
+
+<tr>
+<td>Marital Status:</td>
+<td>
+<select>
+<option>Select</option>
+<option>Married</option>
+<option>Single</option>
+</select>
+</td>
+</tr>
+
+<tr>
+<td>Account Type:</td>
+<td>
+<select>
+<option>Select</option>
+<option>Current</option>
+<option>Savings</option>        
+</select>
+</td>
+</tr>
+
+<tr>
+<td>Initial Deposit Amount:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Mobile Number:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Email Address:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Address:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Occupation:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>National ID (NID):</td>
+<td><input type="text"></td>
+</tr>
+
+
+<tr>
+<td>Set Password:</td>
+<td><input type="text"></td>
+</tr>
+
+<tr>
+<td>Upload ID Proof</td>
+<td><input type="file"></td>
+</tr>
+
+<tr>
+    <td><input type="checkbox">I agree to the terms and conditions
+
+<tr>
+<td></td>
+<td><input type="Submit" value="Register" style="background-color: blue; color:white; padding:8px"  ></td>
+<td><input type="Submit" value="Clear" style="background-color: blue; color:white; padding:8px"></td>
+
+</tr>
+ </table>
+</body>
+ 
+</html>
